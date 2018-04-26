0afb486d447801beda57c154ff52c01919f135fd
JM <jean.d.marchal@gmail.com>
2018-04-26 15:40:38 -0400

    Add metadata_zc

---
+++ R/metadata_zc.R
@@ -0,0 +1,12 @@
+#' Extract metadata from Zero-Crossing files
+#'
+#' @param x an object for which metadata will be extracted
+#'
+#' @export
+#' @rdname metadata_zc
+#'
+
+metadata_zc <- function(x)
+{
+  return(x$metadata)
+}
